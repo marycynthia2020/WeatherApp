faa1de6e58e12d0506ed44829c7c35dbb83dd4dd
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-03-25 07:04:20 +0000

    commit

---
+++ script/location.js
@@ -0,0 +1,42 @@
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(
+//       function(position) {
+//         const latitude = position.coords.latitude;
+//         const longitude = position.coords.longitude;
+//         console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+//       },
+//       function(error) {
+//         console.log("Error getting location: " + error.message);
+//       }
+//     );
+//   } else {
+//     console.log("Geolocation is not supported by this browser.");
+//   }
+
+// function getPosition(position) {
+//   let lat = position.coords.latitude;
+//   let lon = position.coords.longitude;
+
+// }
+let lat;
+let lon;
+const apiKey = "e5931b767b8f27e94d39355ece5c2a2f";
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(position => {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    getWeather()
+  });
+} else{
+        alert(error)
+}
+
+async function getWeather() {
+    if(lat && lon) {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+        const data = await response.json();
+        console.log(data)
+        alert("done")
+    }
+}
